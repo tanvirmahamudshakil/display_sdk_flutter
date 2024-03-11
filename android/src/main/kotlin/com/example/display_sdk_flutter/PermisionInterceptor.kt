1dd0cd627a9271800f9f2fb3c8f496a6e8af91f0
tanvir.yuma <tanvir.yuma@gmail.com>
2024-03-11 13:01:19 +0600

    acascasc

--- android/src/main/kotlin/com/example/display_sdk_flutter/PermisionInterceptor.kt
+++ android/src/main/kotlin/com/example/display_sdk_flutter/PermisionInterceptor.kt
@@ -2,22 +2,22 @@ package com.example.display_sdk_flutter
 
 import android.app.Activity
 import android.widget.Toast
-//import com.hjq.permissions.IPermissionInterceptor
-//import com.hjq.permissions.OnPermissionCallback
+import com.hjq.permissions.IPermissionInterceptor
+import com.hjq.permissions.OnPermissionCallback
 
-//class PermissionInterceptor : IPermissionInterceptor {
-//    override fun grantedPermissions(
-//        activity: Activity, allPermissions: List<String>, grantedPermissions: List<String>,
-//        all: Boolean, callback: OnPermissionCallback
-//    ) {
-//        callback.onGranted(grantedPermissions, all)
-//    }
-//
-//    override fun deniedPermissions(
-//        activity: Activity, allPermissions: List<String>, deniedPermissions: List<String>,
-//        never: Boolean, callback: OnPermissionCallback
-//    ) {
-//        callback.onDenied(deniedPermissions, never)
-//        Toast.makeText(activity, "permission fail",Toast.LENGTH_SHORT).show()
-//    }
-//}
+class PermissionInterceptor : IPermissionInterceptor {
+    override fun grantedPermissions(
+        activity: Activity, allPermissions: List<String>, grantedPermissions: List<String>,
+        all: Boolean, callback: OnPermissionCallback
+    ) {
+        callback.onGranted(grantedPermissions, all)
+    }
+
+    override fun deniedPermissions(
+        activity: Activity, allPermissions: List<String>, deniedPermissions: List<String>,
+        never: Boolean, callback: OnPermissionCallback
+    ) {
+        callback.onDenied(deniedPermissions, never)
+        Toast.makeText(activity, "permission fail",Toast.LENGTH_SHORT).show()
+    }
+}
--- lib/enumvalue.dart
+++ lib/enumvalue.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: constant_identifier_names
+
 enum DisplayType { PD108, PD220, PD280, PD350, PD500, PD700 }
 
 List<int> BaudrateLed = [2400, 4800, 9600];
